459c3e8a4ccc616c38045a86e7100a1fbc64ba53
pjanik <janikpiotrek@gmail.com>
2018-09-04 18:06:27 +0200

    Update plugin demo
    [#160146961]

--- packages/glossary-plugin/src/plugin.tsx
+++ packages/glossary-plugin/src/plugin.tsx
@@ -6,34 +6,46 @@ interface IExternalScriptContext {
   div: any;
 }
 
+const PluginAPI = (window as any).LARA;
+
 export class GlossaryPlugin {
   constructor(authoredState: any, context: IExternalScriptContext) {
-    const div = document.getElementById(context.div.selector);
-    // div itself is hidden, so go higher up. It's just a little hack to present rendering,
-    // not going to be used later.
-    const container = div && div.parentElement && div.parentElement.parentElement;
-
+    const container = document.createElement("div");
     ReactDOM.render(
       <GlossaryPopup
         word="eardrum"
         definition="An eardrum is a membrane, or thin piece of skin, stretched tight like a drum."
         askForUserDefinition={true}
+        onUserDefinitionsUpdate={this.saveUserState}
       />,
       container
     );
+    PluginAPI.openPopup({
+      content: container,
+      title: "Glossary",
+      resizable: false
+    });
+  }
+
+  private saveUserState = (userDefinitions: string[]) => {
+    const userState = {
+      userDefinitions
+    };
+    PluginAPI.saveUserState(this, JSON.stringify(userState));
   }
 }
 
 export const initPlugin = () => {
-  const PluginAPI = (window as any).ExternalScripts;
-  if (!PluginAPI) {
+  // TODO soon it will be replaced with window.LARA. For now use legacy ExternalScripts api to register.
+  const OldPluginAPI = (window as any).ExternalScripts;
+  if (!OldPluginAPI) {
     // LARA Plugin API not available. Nothing to do.
     return;
   }
 
   // tslint:disable-next-line:no-console
   console.log("LARA Plugin API available, GlossaryPlugin initialization");
-  PluginAPI.register("glossary", GlossaryPlugin);
+  OldPluginAPI.register("glossary", GlossaryPlugin);
 };
 
 initPlugin();
