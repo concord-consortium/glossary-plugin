5b3519f795249e704c7258191519907bede8a806
pjanik <janikpiotrek@gmail.com>
2018-09-11 16:03:47 +0200

    Add UI tests
    [#158302666]

---
+++ src/components/plugin-app.test.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import PluginApp from "./plugin-app";
+import { shallow } from "enzyme";
+
+describe("PluginApp component", () => {
+  const mockApi = {
+    decorateContent: jest.fn()
+  }
+});
