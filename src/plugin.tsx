f0861d891d326be357db5704d74caf25eb041300
pjanik <janikpiotrek@gmail.com>
2018-09-27 21:59:13 +0400

    Let authors specify URL pointing to the glossary definition
    [#158302252]

--- src/plugin.tsx
+++ src/plugin.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import PluginApp from "./components/plugin-app";
+import "whatwg-fetch"; // window.fetch polyfill for older browsers (IE)
 
 interface IExternalScriptContext {
   div: any;
@@ -11,43 +12,74 @@ interface IExternalScriptContext {
 
 let PluginAPI: any;
 
-const fallbackLearnerState = { definitions: {} };
+const getAuthoredState = async (context: IExternalScriptContext) => {
+  if (!context.authoredState) {
+    return {};
+  }
+  let authoredState;
+  try {
+    authoredState = JSON.parse(context.authoredState);
+  } catch (error) {
+    // tslint:disable-next-line:no-console
+    console.warn("Unexpected authoredState:", context.authoredState);
+    return {};
+  }
+  // Authored state can contain all the necessary data already or specify only URL that points to a proper state.
+  if (typeof authoredState.url === "string") {
+    const response = await fetch(authoredState.url);
+    try {
+      const textResponse = await response.text();
+      return JSON.parse(textResponse);
+    } catch (error) {
+      // tslint:disable-next-line:no-console
+      console.warn("Unexpected/malformed authoredState at URL:", authoredState.url);
+      return {};
+    }
+  } else {
+    return authoredState;
+  }
+};
+
+const getLearnerState = (context: IExternalScriptContext) => {
+  const fallbackLearnerState = { definitions: {} };
+  if (!context.learnerState) {
+    return fallbackLearnerState;
+  }
+  try {
+    return JSON.parse(context.learnerState);
+  } catch (error) {
+    // tslint:disable-next-line:no-console
+    console.warn("Unexpected learnerState:", context.learnerState);
+    return fallbackLearnerState;
+  }
+};
 
 export class GlossaryPlugin {
+  public context: IExternalScriptContext;
   public pluginAppComponent: any;
 
   constructor(context: IExternalScriptContext) {
-    let authoredState;
-    if (!context.authoredState) {
-      authoredState = {};
-    } else {
-      try {
-        authoredState = JSON.parse(context.authoredState);
-      } catch (error) {
-        // tslint:disable-next-line:no-console
-        console.warn("Unexpected authoredState:", context.authoredState);
-        authoredState = {};
-      }
-    }
+    this.context = context;
+    // Note renderPluginApp is an async function. Constructor can't be async, as it needs to return immediately.
+    // It also means that component won't be rendered immediately. That's fine.
+    this.renderPluginApp();
+  }
+
+  public renderPluginApp = async () => {
+    const authoredState = await getAuthoredState(this.context);
     const definitions = authoredState.definitions || [];
     const askForUserDefinition = authoredState.askForUserDefinition || false;
-    let initialLearnerState;
-    if (!context.learnerState) {
-      initialLearnerState = fallbackLearnerState;
-    } else {
-      try {
-        initialLearnerState = JSON.parse(context.learnerState);
-      } catch (error) {
-        // tslint:disable-next-line:no-console
-        console.warn("Unexpected learnerState:", context.learnerState);
-        initialLearnerState = fallbackLearnerState;
-      }
+    const initialLearnerState = getLearnerState(this.context);
+
+    if (this.pluginAppComponent) {
+      ReactDOM.unmountComponentAtNode(this.context.div);
+      this.pluginAppComponent = undefined;
     }
 
     this.pluginAppComponent = ReactDOM.render(
       <PluginApp
         PluginAPI={PluginAPI}
-        pluginId={context.pluginId}
+        pluginId={this.context.pluginId}
         definitions={definitions}
         initialLearnerState={initialLearnerState}
         askForUserDefinition={askForUserDefinition}
@@ -55,7 +87,7 @@ export class GlossaryPlugin {
       // It can be any other element in the document. Note that PluginApp render everything using React Portals.
       // It renders child components into external containers sent to LARA, not into context.div
       // or anything else that we provide here.
-      context.div);
+      this.context.div);
   }
 }
 
