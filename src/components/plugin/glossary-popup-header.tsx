60f252c0b4b7c477c402fb6173af469dcf9eef1b
lublagg <lvblagg@gmail.com>
2022-04-26 17:51:47 -0400

    Added translations to icons and header, adjusted language toggle. [#181870763]

---
+++ src/components/plugin/glossary-popup-header.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+import * as css from "./glossary-popup.scss";
+
+export interface ILanguage {
+  lang: string;
+  selected: boolean;
+}
+
+interface IProps {
+  languages?: ILanguage[];
+	word: string;
+	translatedWord: string;
+}
+
+export default class GlossaryPopupHeader extends React.Component<IProps, {}> {
+
+  public render() {
+    const { languages, word, translatedWord } = this.props;
+		const languagesNotPresent = !languages || (languages.length === 0)
+
+    if (!languages || (languages.length === 0) ) {
+			return (
+				<div className={css.header}>
+					<h4>Term: {word}</h4>
+				</div>
+			)
+    }
+
+		const en = languages.filter(language => language.lang === "en")[0];
+
+    return (
+      <div className={css.header}>
+        <h4>Term: {word} {en.selected ? "" : `(${translatedWord})`}</h4>
+      </div>
+    );
+  }
+
+}
