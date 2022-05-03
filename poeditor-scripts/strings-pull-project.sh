d0c266dc4c07f84630a4ed2bdce075a3028c0c04
lublagg <lvblagg@gmail.com>
2022-05-03 13:01:28 -0400

    Added Hawaiian translations and updated all languages. [#182054652]

--- poeditor-scripts/strings-pull-project.sh
+++ poeditor-scripts/strings-pull-project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROJECT_ID=297867
 OUTPUT_DIR=src/lang
-LANGUAGES=("es" "pt" "ar" "zh-CN" "ru" "gv" "mi" "mr")
+LANGUAGES=("es" "pt" "ar" "zh-CN" "ru" "gv" "mi" "mr" "mh")
 
 # argument processing from https://stackoverflow.com/a/14203146
 while [[ $# -gt 1 ]]
